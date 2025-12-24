f2741868f7af01180083d5384ef5c33b8f3f5a3d
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2025-12-24 18:05:59 +0100

    add index php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bakery Goods</title>
+  <meta name="description" content="Fresh pastry and bread made to order.">
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'header.php'; ?>
+</body>
+</html>
+
