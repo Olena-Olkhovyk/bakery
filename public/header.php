00060aa9a0ae9e39fd3304a454b3715a22b3dc3b
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2025-12-24 18:07:14 +0100

    feat: add header

---
+++ public/header.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <div class="header-container">
+        <div>
+             <p class="header-title">BAKERY GOODS</p>
+        </div>
+        <div>
+            <img src="./assets/cart.png" alt="cart" class="cart-image">
+        </div>
+    </div>
+</header>
