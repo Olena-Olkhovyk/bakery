2d326ea1d1a6a79b31a142499c70d9d5ba5a90f9
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2025-12-29 15:27:00 +0100

    add content file

---
+++ public/content.php
@@ -0,0 +1,24 @@
+<main class="content-container">
+    <div class="content-wrapper">
+      <div>
+        <p class="hero-title">The best pastry made from fresh berries </p>
+        <button class="hero-btn">Buy pastry</button>
+      </div>
+      <div>
+        <img src="./assets/hero.png" alt="hero" class="hero-image">
+      </div>
+    </div>
+    <div class="content-wrapper">
+      <div>
+        <img src="./assets/bread.png" alt="bread" class="bread-image">
+      </div>
+      <div>
+        <h2 class="bread-title">Fresh-Baked Bread Made to Order </h2>
+        <p>Say goodbye to stale supermarket bread. Our handcrafted baton is baked fresh every day using only natural ingredients and traditional baking methods. Order your loaf with delivery and receive it warm right at your door — perfect for breakfast, dinner, or any occasion. Choose your preferred delivery time and let us handle the rest.</p>
+        <button class="hero-btn">Buy pastry</button>
+      </div>
+    </div>
+    <div>
+        <h3>The steps to order our pastry</h3>
+    </div>
+</main>
