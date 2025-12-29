8d208ac11e2283d04d6777994f1b8607c2393440
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2025-12-29 15:26:00 +0100

    change class

--- public/footer.php
+++ public/footer.php
@@ -8,7 +8,7 @@
              <img src="./assets/insta.png" alt="instagram" class="insta">
              <img src="./assets/twitter.png" alt="twitter" class="twitter">
         </div>
-        <div class="footer-container">
+        <div class="footer-content">
             <div>
                 <p>Our phone number:</p>
                 <p>+49 155 507 00 00</p>
