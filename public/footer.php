659f967d45dbb5ebef53d1df3e6d5f546f585220
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2025-12-29 12:45:05 +0100

    add footer

---
+++ public/footer.php
@@ -0,0 +1,26 @@
+<footer class="site-container footer">
+    <div class="footer-container">
+        <div>
+             <p class="logo-title">BAKERY GOODS</p>
+        </div>
+        <div class="footer-images">
+             <img src="./assets/facebook.png" alt="facebook" class="facebook">
+             <img src="./assets/insta.png" alt="instagram" class="insta">
+             <img src="./assets/twitter.png" alt="twitter" class="twitter">
+        </div>
+        <div class="footer-container">
+            <div>
+                <p>Our phone number:</p>
+                <p>+49 155 507 00 00</p>
+            </div>
+             <div>
+                <p>Our address:</p>
+                <p>Rosmarinnenstrasse 29, 13864 Berlin</p>
+            </div>
+             <div>
+                <p>Our email address:</p>
+                <p>bakery@goods.de</p>
+            </div>
+        </div>
+    </div>
+</footer>
